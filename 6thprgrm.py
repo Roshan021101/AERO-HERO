95b5922d0a07869eab295e1bda34688b5b19d4c5
Roshan021101 <roshankami670@gmail.com>
2026-06-08 17:30:28 +0530

    Refactor code to handle missing data and scale features

--- 6thprgrm.py
+++ 6thprgrm.py
@@ -1,3 +1,5 @@
+# write a program to handle missing data encode categorial variablesand program featurescaling 
+
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder, StandardScaler
